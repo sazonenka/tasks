582864381317e8915337aa2c80c082db8370d1d9
sazonenka <aliaksandr.sazonenka@gmail.com>
2017-06-19 14:27:39 -0700

    Add solutions for some problem using greedy algorithms

---
+++ src/MaximizingNumberOfPrizePlaces.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class MaximizingNumberOfPrizePlaces {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+
+    List<Integer> result = new ArrayList<>();
+    for (int i = 1; ; i++) {
+      if (2*i < n) {
+        result.add(i);
+        n -= i;
+      } else {
+        result.add(n);
+        break;
+      }
+    }
+    System.out.println(result.size());
+    System.out.println(String.join(" ",
+        result.stream().map(Object::toString).collect(Collectors.toList())));
+  }
+}
---
+++ src/MaximizingYourSalary.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaximizingYourSalary {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+
+    Piece[] pieces = new Piece[n];
+    for (int i = 0; i < n; i++) {
+      pieces[i] = new Piece(scanner.nextInt());
+    }
+
+    Arrays.sort(pieces);
+
+    StringBuilder answer = new StringBuilder();
+    for (Piece piece : pieces) {
+      answer.append(piece.number);
+    }
+    System.out.println(answer);
+  }
+
+  private static class Piece implements Comparable<Piece> {
+    final int number;
+
+    Piece(int number) {
+      this.number = number;
+    }
+
+    @Override
+    public int compareTo(Piece p) {
+      int sum1 = Integer.parseInt(String.valueOf(number) + String.valueOf(p.number));
+      int sum2 = Integer.parseInt(String.valueOf(p.number) + String.valueOf(number));
+      return Integer.compare(sum2, sum1);
+    }
+  }
+}
